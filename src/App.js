4aee07079879079bdc27e89f352a6ddeebf248b5
Dan VC <danvc@ellenmacarthurfoundation.org>
2019-11-25 09:37:28 +0000

    Set up and removal of standard RCA stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Toaster</h1>
+    {
+      /* 
+        Toaster. A few pieces of state – on/off, light/dark. A timer
+        1. On/off state
+        2. Light/Dark state. 
+        3. A timer
+      */
+    }
+  </div>
+);
 
 export default App;
