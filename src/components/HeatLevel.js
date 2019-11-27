f5170f80fd50e32392c7087faaac5698417767fe
Dan VC <danvc@ellenmacarthurfoundation.org>
2019-11-27 09:53:02 +0000

    Remove warning after heat level status change

--- src/components/HeatLevel.js
+++ src/components/HeatLevel.js
@@ -7,7 +7,7 @@ class HeatLevel extends Component {
       <HeatLevelBar theme={this.props.theme}>
         <li>
           <button 
-            className={this.props.toastLevel === "lightlyDone" && "active"}
+            className={`${this.props.toastLevel === "lightlyDone" && "active"}`}
             onClick={this.props.heatClick}
             data-toast-level="lightlyDone"
             data-toast-level-time="4"
@@ -17,7 +17,7 @@ class HeatLevel extends Component {
         </li>
         <li>
           <button
-            className={this.props.toastLevel === "mediumDone" && "active"}
+            className={`${this.props.toastLevel === "mediumDone" && "active"}`}
             onClick={this.props.heatClick}
             data-toast-level="mediumDone"
             data-toast-level-time="7"
@@ -27,7 +27,7 @@ class HeatLevel extends Component {
         </li>
         <li>
           <button
-            className={this.props.toastLevel === "overDone" && "active"}
+            className={`${this.props.toastLevel === "overDone" && "active"}`}
             onClick={this.props.heatClick}
             data-toast-level="overDone"
             data-toast-level-time="10"
