e13a3c529cdb44daa9b0b10c76f14967b24abc28
Dan VC <danvc@ellenmacarthurfoundation.org>
2019-11-28 12:13:15 +0000

    Convert HeatLevel class comp to a stateless comp

--- src/components/HeatLevel.js
+++ src/components/HeatLevel.js
@@ -1,49 +1,47 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { HeatLevelBar } from './HeatLevel.css'
 
-class HeatLevel extends Component {
-  render() {
-    return (
-      <HeatLevelBar 
-        theme={this.props.theme}
-        data-testid="heatlevels"
-      >
-        <li>
-          <button 
-            className={`${this.props.toastLevel === "lightlyDone" && "active"}`}
-            onClick={this.props.heatClick}
-            data-toast-level="lightlyDone"
-            data-toast-level-time="4"
-            disabled={this.props.isOn || this.props.toasting}
-          >
-            <span>Light</span>
-          </button>
-        </li>
-        <li>
-          <button
-            className={`${this.props.toastLevel === "mediumDone" && "active"}`}
-            onClick={this.props.heatClick}
-            data-toast-level="mediumDone"
-            data-toast-level-time="7"
-            disabled={this.props.isOn || this.props.toasting}
-          >
-            <span>Medium</span>
-          </button>
-        </li>
-        <li>
-          <button
-            className={`${this.props.toastLevel === "overDone" && "active"}`}
-            onClick={this.props.heatClick}
-            data-toast-level="overDone"
-            data-toast-level-time="10"
-            disabled={this.props.isOn || this.props.toasting}
-          >
-            <span>Well done</span>
-          </button>
-        </li>
-      </HeatLevelBar>
-    );
-  }
+const HeatLevel = (props) => {
+  return (
+    <HeatLevelBar 
+      theme={props.theme}
+      data-testid="heatlevels"
+    >
+      <li>
+        <button 
+          className={`${props.toastLevel === "lightlyDone" && "active"}`}
+          onClick={props.heatClick}
+          data-toast-level="lightlyDone"
+          data-toast-level-time="4"
+          disabled={props.isOn || props.toasting}
+        >
+          <span>Light</span>
+        </button>
+      </li>
+      <li>
+        <button
+          className={`${props.toastLevel === "mediumDone" && "active"}`}
+          onClick={props.heatClick}
+          data-toast-level="mediumDone"
+          data-toast-level-time="7"
+          disabled={props.isOn || props.toasting}
+        >
+          <span>Medium</span>
+        </button>
+      </li>
+      <li>
+        <button
+          className={`${props.toastLevel === "overDone" && "active"}`}
+          onClick={props.heatClick}
+          data-toast-level="overDone"
+          data-toast-level-time="10"
+          disabled={props.isOn || props.toasting}
+        >
+          <span>Well done</span>
+        </button>
+      </li>
+    </HeatLevelBar>
+  )
 }
 
 export default HeatLevel;
